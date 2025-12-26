43209e6bbd1c2822893d856d4480511e1b77ec87
Patrick Hany <patrickhany990@gmail.com>
2025-12-26 23:44:47 +0200

    Add foreign key references to announcement attributes

--- Backend/models/announcementAttributeValue.js
+++ Backend/models/announcementAttributeValue.js
@@ -1,6 +1,9 @@
 const { DataTypes } = require('sequelize');
 const { sequelize } = require('../db');
 
+const Announcement = require('./announcement');
+const AnnouncementAttribute = require('./announcementAttribute');
+
 // Stores attribute values for announcements (EAV value table)
 const AnnouncementAttributeValue = sequelize.define('AnnouncementAttributeValue', {
   id: {
@@ -11,15 +14,33 @@ const AnnouncementAttributeValue = sequelize.define('AnnouncementAttributeValue'
   announcementId: {
     type: DataTypes.INTEGER,
     allowNull: false,
+    references: {
+      model: Announcement,
+      key: 'id',
+    },
+    onUpdate: 'CASCADE',
+    onDelete: 'CASCADE',
   },
   attributeId: {
     type: DataTypes.INTEGER,
     allowNull: false,
+    references: {
+      model: AnnouncementAttribute,
+      key: 'id',
+    },
+    onUpdate: 'CASCADE',
+    onDelete: 'CASCADE',
   },
   value: {
     type: DataTypes.TEXT,
     allowNull: true,
   },
+}, {
+  indexes: [
+    { unique: true, fields: ['announcementId', 'attributeId'] },
+    { fields: ['announcementId'] },
+    { fields: ['attributeId'] },
+  ],
 });
 
 module.exports = AnnouncementAttributeValue;
